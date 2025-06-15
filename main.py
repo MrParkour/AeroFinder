ff9db91698f99bcd12bf7a93b57afed6ab71515f
Iolov Lev <leon219ru@yandex.ru>
2025-06-15 16:26:43 +0300

    start wright checking comboboxs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from PyQt5 import QtWidgets, uic
 import sys
+import sqlite3
+
+connection = sqlite3.connect("data/plane_db.db")
+cursor = connection.cursor()
  
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("ui_desingnes/ui_design_V_4.ui")
@@ -35,19 +39,86 @@ def prev_page_plane():
     else:
         obj.setCurrentIndex(obj.currentIndex() - 1)
 
+
+def finding():
+    # активируетс япри нажатии на кнопку найти
+    # далее проверяется на какой странице (самолёты, вертолёты, все) была нажата кнопка
+    button = QtWidgets.QApplication.instance().sender()
+    if button == ui.pushButton_find_p:  
+        # finding planes
+        # если флажок россия стоит то в вида строки добавить россия
+        plans_country_check = False
+        plane_country_array = []
+        if ui.P_coun_cb_Rus.isChecked():
+            plans_country_check = True
+            plane_country_array.append("Россия")
+        if ui.P_coun_cb_USSR.isChecked():
+            plans_country_check = True
+            plane_country_array.append("СССР")
+
+    elif button == ui.pushButton_find_h:
+        # finding helicopters
+        obj = ui.stackedWidget_heli
+    elif button == ui.push_find_all:
+        # finding all
+        obj = ui.stackedWidget_all
+    
+    # сделать проверку на флажки
+    # записать итог в строку вида "спортивный, перехватчик, истребитель"
+    # добавить переменную bool которая показывается есть ли чтото или нет
+
+
+    exc = """SELECT * FROM aircrafts
+WHERE TYPE IN () 
+AND COUNTRY IN () 
+AND YEAR BETWEEN X*0.9 AND X*1.1
+AND APPLICATION IN ("")
+AND PURPOSE
+AND ENGINE_NAME
+AND ENGINE_TYPE
+AND ENGINE_ROD
+AND ENGINE_FORSAGE_ROD
+AND ENGINE_CONSUMPTION
+AND MASS_EMPTY
+AND MASS_NORMAL
+AND MASS_MAXIMAL
+AND LENGTH
+AND HEIGHT
+AND WING_SPAN
+AND WING_AREA
+AND WING_SWEEP
+AND WHEEL_BASE
+AND WHEEL_TRACK
+AND SPEED_MAX
+AND SPEED_NORMAL
+AND RANGE
+AND HEIGHT_MAX
+AND SERVICE_CEILING
+AND STATIC_SEILING
+AND CREW
+AND ARMAMENT
+AND PASSENGERS
+AND PAYLOAD;"""
+    cursor.execute(exc)
+    output = cursor.fetchall()
+    if output != []:
+        # vivod
+        pass
+    else:
+        # grusniy smalik
+        pass
+
 ui.pushButton_forward_p.clicked.connect(next_page_plane)
 ui.pushButton_back_p.clicked.connect(prev_page_plane)
 ui.pushButton_forward_h.clicked.connect(next_page_plane)
 ui.pushButton_back_h.clicked.connect(prev_page_plane)
 ui.push_forward_all.clicked.connect(next_page_plane)
 ui.push_back_all.clicked.connect(prev_page_plane)
+ui.push_find_all.clicked.connect(finding)
+ui.pushButton_find_p.clicked.connect(finding)
+ui.pushButton_find_h.clicked.connect(finding)
 
-# def button_prev_page():
-#     print("prev page")
-#     ui.stackedWidget.setCurrentIndex(0)
-
-# ui.pushButton_forward.clicked.connect(button_next_page)
-# ui.pushButton_back.clicked.connect(button_prev_page)
 
 ui.show()
 sys.exit(app.exec())
+connection.close()
