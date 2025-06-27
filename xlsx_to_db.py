0731d35643be349aa7513f1ee32275fdea0a73a8
z1xes <mr-parkour.yt@yandex.ru>
2025-06-28 01:19:14 +0300

    slightly final

--- xlsx_to_db.py
+++ xlsx_to_db.py
@@ -1,8 +1,8 @@
 import sqlite3
 import pandas as pd
 
-cxn = sqlite3.connect('database/plane_db copy.db')
-wb = pd.read_excel('database/db_excel_2.xlsx',sheet_name = 'Лист1')
-wb.to_sql(name='aircrafts_2',con=cxn,if_exists='replace',index=True)
+cxn = sqlite3.connect('database/plane_db.db')
+wb = pd.read_excel('database/db_excel_3.xlsx',sheet_name = 'Лист1')
+wb.to_sql(name='aircrafts',con=cxn,if_exists='replace',index=True)
 cxn.commit()
 cxn.close()
